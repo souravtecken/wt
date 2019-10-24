2d83c42355cb626dad79a49d4291830e1f49d3c9
souravtecken <souravtecken@gmail.com>
2019-10-24 22:27:26 +0530

    Added timetable, and accordion effect for bcalendar. - Added jquery and boostrap-min.js

---
+++ home.js
@@ -0,0 +1,20 @@
+$(document).ready(() => {    
+    $('.timetable-toggle').click((event) => {
+        const organisationTimetable = $('#organisation-timetable');
+        const personalTimetable = $('#personal-timetable');
+        const clickedId = event.target.id;
+        if(clickedId === 'personal-timetable-tab'){            
+            $('#organisation-timetable-tab').removeClass('active');
+            personalTimetable.show();
+            organisationTimetable.hide();
+        }            
+        else{
+            $('#personal-timetable-tab').removeClass('active');
+            personalTimetable.hide();
+            organisationTimetable.show();
+        }            
+        event.target.classList.add('active');
+    })
+    // Enable bootstrap accordions
+    $('.collapse').collapse();
+})
